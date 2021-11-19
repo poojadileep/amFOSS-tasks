de9e850434dc9c4d2f4e8d9969437d80f93e522e
Pooja Dileep <92637957+poojadileep@users.noreply.github.com>
2021-11-19 22:24:14 +0530

    Add files via upload

---
+++ task-08/page 2/index.js
@@ -0,0 +1,29 @@
+
+
+function TimePopUp(){
+      alert(new Date());
+}
+
+function changecolor(){
+       document.body.style.background = 'red';
+}
+
+     function console1(){
+            console.log('I did it');
+     }
+     
+     function reload(){
+       window.location.reload();     
+     }
+
+     function newwindow(){
+       window.open("https://www.google.com");
+     }
+
+
+   function store(){
+       window.localStorage.setItem("click","button");  
+   }
+
+   
+   
